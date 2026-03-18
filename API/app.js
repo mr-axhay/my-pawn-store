9ab8fac78f204d36cd16655c0163bd90ff8d549c
mr-axhay <mr.axhay@gmail.com>
2026-03-19 03:01:07 +0530

    initial commit with fe and BE

---
+++ API/app.js
@@ -0,0 +1,48 @@
+import 'dotenv/config';
+import express from 'express';
+import Razorpay from 'razorpay';
+import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
+import cors from 'cors';
+import UserRouter from './routes/user.router.js';
+import CategoryRouter from './routes/category.router.js';
+import SubCategoryRouter from './routes/subcategory.js';
+import ForgetPassword from './controller/fp.controller.js';
+import ProductRouter from './routes/product.router.js';
+import aiChatRoute from "./routes/aiChat.js";
+import chatRouter from "./routes/chat.router.js";
+import paymentRoutes from "./routes/payment.routes.js";
+
+const razorpayInstance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+});
+
+const app = express();
+
+
+//to handle cross origin request
+app.use(cors());
+
+//configuration to fetch req body content : body parser middleware
+//used to fetch req data from methods like : POST , PUT , PATCH , DELETE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//configuration to fetch file content : file upload middleware
+app.use(fileUpload());
+
+//route level middleware
+app.use("/chat", chatRouter);
+app.use("/api/ai", aiChatRoute);
+app.use("/user", UserRouter);
+app.use("/category", CategoryRouter);
+app.use("/subcategory", SubCategoryRouter);
+//route for forgetpassword
+app.post("/forgetpassword", ForgetPassword);
+app.use("/api/payment", paymentRoutes);
+app.use("/product", ProductRouter);
+app.listen(3001);
+console.log("Server invoked at link http://localhost:3001");
+
+
---
+++ API/routes/payment.routes.js
@@ -0,0 +1,40 @@
+import express from "express";
+import crypto from "crypto";
+const router = express.Router();
+import razorpay from '../config/razorpay.js';
+
+router.post("/create-order", async (req, res) => {
+  try {
+    const { amount } = req.body;
+
+    const options = {
+      amount: amount * 100, // amount in paise
+      currency: "INR",
+      receipt: "receipt_order_1",
+    };
+
+    const order = await razorpay.orders.create(options);
+
+    res.json(order);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+router.post("/verify-payment", (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .update(body.toString())
+    .digest("hex");
+
+  if (expectedSignature === razorpay_signature) {
+    res.json({ success: true });
+  } else {
+    res.status(400).json({ success: false });
+  }
+});
+
+ export default router;
