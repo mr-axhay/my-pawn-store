6624fd5e5d035ceb6bcd9f4c4ae7a8ab5b3240af
mr-axhay <mr.axhay@gmail.com>
2026-03-25 00:04:08 +0530

    remove extra connect file

--- API/models/connection.js
+++ API/models/connection.js
@@ -1,8 +1,9 @@
 //database connection file : mongodb + express
 //for connection 'mongoose' client is used
 
-import mongoose from 'mongoose';
-const url="mongodb://127.0.0.1:27017/pawnshop";
-mongoose.connect(url);
+// import mongoose from 'mongoose';
+// const url="mongodb://127.0.0.1:27017/pawnshop";
+// mongoose.connect(url);
 // mongoose.connect(process.env.MONGO_URI);
-console.log("Successfully connected to mongodb database...");
+// console.log("Successfully connected to mongodb database...");
+// 
