a0abe3a59586713b149bfde82fb4d2ce0a89fd92
mr-axhay <mr.axhay@gmail.com>
2026-03-25 03:09:20 +0530

    test commit

--- API/controller/fp.controller.js
+++ API/controller/fp.controller.js
@@ -1,35 +1,35 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 const ForgetPassword = (req, res) => {
   const email = req.body.email;
   let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      user: 'vilekhofficial@gmail.com',
-      pass: 'olqxfaikwimxxkzu'
-    }
+      user: "vilekhofficial@gmail.com",
+      pass: "olqxfaikwimxxkzu",
+    },
   });
 
   let mailOptions = {
-    from: 'vilekhofficial@gmail.com',
+    from: "vilekhofficial@gmail.com",
     to: email,
-    subject: 'Link For ForgetPassword PawnShop',
+    subject: "Link For ForgetPassword PawnShop",
     html: `
-  <h1>Welcome to pawnshop</h1>
+  <h1>Welcome to Pawn Shop</h1>
   <h2>Click below to reset password</h2>
   <a href='${process.env.FRONTEND_URL}/reset-password/${email}'>
     Click to reset password
   </a>
-`
+`,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
-      res.status(200).json({ "status": true });
+      console.log("Email sent: " + info.response);
+      res.status(200).json({ status: true });
     }
   });
-}
+};
 export default ForgetPassword;
