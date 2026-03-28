2bb644e1d46ff306980ea2635e86564e4c172401
mr-axhay <mr.axhay@gmail.com>
2026-03-28 17:43:31 +0530

    update options on admin and user home , add cat name on sub cat

--- UI/src/components/AdminHomePage.jsx
+++ UI/src/components/AdminHomePage.jsx
@@ -89,28 +89,17 @@ export const BentoCard = ({ src, title, description, isComingSoon }) => {
 const AdminHomePage = () => (
   <section className="bg-black pb-52">
     <div className="container mx-auto px-3 md:px-10">
-      {/* <div className="px-5 py-32">
-        <p className="font-circular-web text-lg text-blue-50">
+      <div className="px-4 py-4">
+        <p className="font-circular-web text-2xl text-blue-50">
           Welcome To Admin Panel
         </p>
-        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-          Step into a dynamic marketplace where a diverse range of items—from timeless
-          valuables to everyday essentials—come together in a seamless pawn shop experience,
-          connecting your needs with opportunity in one place.
-        </p>
-      </div> */}
+      </div>
 
       <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
         <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
           <BentoCard
-            src="https://img.freepik.com/free-photo/closeup-scarlet-macaw-from-side-view-scarlet-macaw-closeup-head_488145-3540.jpg?semt=ais_incoming&w=740&q=80"
-            title={
-              <>
-                 Welcome <b> To Admin </b> Panel
-              </>
-            }
-            // description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
-            isComingSoon
+            src="img/user1.jpg"
+          isComingSoon
           />
         </BentoTilt>
 
@@ -129,7 +118,7 @@ const AdminHomePage = () => (
 
         <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
           <BentoCard
-            src="img/logo.png"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2fjXAnhJL0kvh3p-Qpcz4ZxaMTqukNEEAVA&s"
             title={
               <>
                 Add<b> Categories</b>
@@ -141,7 +130,7 @@ const AdminHomePage = () => (
         </BentoTilt>
         <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
           <BentoCard
-            src="videos/settings.mp"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2fjXAnhJL0kvh3p-Qpcz4ZxaMTqukNEEAVA&s"
             title={
               <>
                 Add <b>Sub Categories</b>
@@ -153,7 +142,7 @@ const AdminHomePage = () => (
         </BentoTilt>
           <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
           <BentoCard
-            src="videos/settings.mp"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2fjXAnhJL0kvh3p-Qpcz4ZxaMTqukNEEAVA&s"
             title={
               <>
                 <b>Settings</b>
@@ -163,39 +152,6 @@ const AdminHomePage = () => (
             isComingSoon
           />
         </BentoTilt>
-        {/* <BentoTilt className="bento-tilt_2">
-          <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
-            <h1 className="bento-title special-font max-w-64 text-black">
-              S<b>o</b>on will <b>be </b>on Mo<b>bile</b>Apps.
-            </h1>
-
-            <TiLocationArrow className="m-5 scale-[5] self-end" />
-          </div>
-        </BentoTilt> */}
-
-        {/* <BentoTilt className="bento-tilt_2">
-          <video
-            src="videos/feature-5.mp"
-            loop
-            muted
-            autoPlay
-            className="size-full object-cover object-center"
-          />
-        </BentoTilt> */}
-        
-      {/* <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
-        <BentoCard
-          src="videos/featur.mp4"
-          title={
-            <>
-              Welcome <b>To </b>Admin Panel
-            </>
-          }
-          // description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
-          // isComingSoon
-        />
-      </BentoTilt> */}
-
       </div>
     </div>
   </section>
