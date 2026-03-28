ddf0fb2f6e7b18270505847762d96b7fbb03919d
mr-axhay <mr.axhay@gmail.com>
2026-03-29 04:15:33 +0530

    smtp fr fp controller changed

--- API/controller/fp.controller.js
+++ API/controller/fp.controller.js
@@ -1,37 +1,3 @@
-// import nodemailer from "nodemailer";
-
-// const ForgetPassword = (req, res) => {
-//   const email = req.body.email;
-//   let transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-//   let mailOptions = {
-//     from: process.env.EMAIL_USER,
-//     to: email,
-//     subject: "Link For ForgetPassword PawnShop",
-//     html: `
-//   <h1>Welcome to Pawn Shop</h1>
-//   <h2>Click below to Reset Password</h2>
-//   <a href='${process.env.FRONTEND_URL}/reset-password/${encodeURIComponent(email)}'>
-//     Click to reset password
-//   </a>
-// `,
-//   };
-
-//   transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log("Email sent:: " + info.response);
-//       res.status(200).json({ status: true });
-//     }
-//   });
-// };
-// export default ForgetPassword;
 import nodemailer from "nodemailer";
 
 const ForgetPassword = async (req, res) => {
